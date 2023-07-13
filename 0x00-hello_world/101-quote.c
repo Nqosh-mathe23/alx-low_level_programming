c9c55fdd0e6884c3e06ae68dd672ac0fe5d75c00
Nqosh-mathe23 <lethu.mathebula@gmail.com>
2023-07-13 14:40:21 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <unstd.h>
+#include <unistd.h>
 /**
  * main - A C program that prints a line to the standard error 
  * Return 1 (Success)
@@ -7,5 +7,5 @@
 int main(void)
 {
 	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"stdout);
-	return (1);
+return (1);
 }
